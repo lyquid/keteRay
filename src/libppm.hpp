6cfe3b5757bd96fdeacbcd8c768373e74273f36e
Alex CB <ketepincho@gmail.com>
2022-10-19 18:25:27 +0200

    Added Color type

--- src/libppm.hpp
+++ src/libppm.hpp
@@ -19,7 +19,12 @@
 
 namespace libppm {
 
-using Pixels = std::vector<int>;
+struct Color {
+  Color(int red, int green, int blue): r {red}, g {green}, b {blue} {}
+  int r{}, g{}, b{};
+};
+
+using Pixels = std::vector<Color>;
 
 struct PPMFileData {
   const int   m_channels_per_color {3};
@@ -34,13 +39,10 @@ void makePPMFile(const PPMFileData& data) {
   // header for ppm file
   image_file << "P3\n" << data.m_width << ' ' << data.m_height << "\n255\n";
   // write pixels to file. 1 RGB triplet per row
-  for (auto i = 0u; i < data.m_pixels.size(); i += data.m_channels_per_color) {
-    for (auto j = 0; j < data.m_channels_per_color; ++j) {
-      image_file << data.m_pixels[i + j];
-      // if not the last column, add a space
-      if (j != data.m_channels_per_color - 1) image_file << ' ';
-    }
-    image_file << '\n';
+  for (auto i = 0u; i < data.m_pixels.size(); ++i) {
+    image_file << data.m_pixels[i].r << ' ';
+    image_file << data.m_pixels[i].g << ' ';
+    image_file << data.m_pixels[i].b << '\n';
   }
   image_file.close();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,19 @@ int main(int argc, char* argv[]) {
   data.m_name = "render.ppm";
   data.m_height = 1024;
   data.m_width  = 1024;
-  data.m_pixels.reserve(data.m_width * data.m_height * data.m_channels_per_color);
+  data.m_pixels.reserve(data.m_width * data.m_height);
 
   // pixels
   for (auto row = data.m_height - 1; row >= 0; --row) {
     std::cout << "\rScanlines remaining: " << row << ' ' << std::flush;
     for (auto col = 0; col < data.m_width; ++col) {
-      const     double r {static_cast<double>(col) / (data.m_width  - 1)};
-      const     double g {static_cast<double>(row) / (data.m_height - 1)};
-      constexpr double b {0.25};
-
       constexpr auto magic_num {255.999};
-      const int ir {static_cast<int>(magic_num * r)};
-      const int ig {static_cast<int>(magic_num * g)};
-      const int ib {static_cast<int>(magic_num * b)};
-
-      data.m_pixels.push_back(ir);
-      data.m_pixels.push_back(ig);
-      data.m_pixels.push_back(ib);
+      const libppm::Color i_color {
+        static_cast<int>(magic_num * static_cast<double>(col) / (data.m_width  - 1)),
+        static_cast<int>(magic_num * static_cast<double>(row) / (data.m_height - 1)),
+        static_cast<int>(magic_num * 0.25)
+      };
+      data.m_pixels.push_back(i_color);
     }
   }
   std::cout << "\rScanlines processing finished.\n";
