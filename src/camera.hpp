0984d17b1b7fdc7e87fbdcc19b5d677977261891
Alex CB <ketepincho@gmail.com>
2022-10-25 16:06:14 +0200

    Camera class

--- src/camera.hpp
+++ src/camera.hpp
@@ -12,9 +12,27 @@
 #ifndef KTP_CAMERA_HPP_
 #define KTP_CAMERA_HPP_
 
+#include "owert.hpp"
+#include "ray.hpp"
+
 namespace ktp {
 
-class Camera {};
+class Camera {
+ public:
+ auto aspectRatio() const { return aspect_ratio; }
+  Ray getRay(double u, double v) const {
+    return Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+  }
+ private:
+  double aspect_ratio {16.0 / 9.0};
+  double viewport_height {2.0};
+  double viewport_width {aspect_ratio * viewport_height};
+  double focal_length {1.0};
+  Point origin {0.0, 0.0, 0.0};
+  Vector horizontal {viewport_width, 0.0, 0.0};
+  Vector vertical {0.0, viewport_height, 0.0};
+  Point lower_left_corner {origin - horizontal / 2.0 - vertical / 2.0 - Vector(0.0, 0.0, focal_length)};
+};
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,28 @@
+#include "camera.hpp"
 #include "hittable.hpp"
 #include "owert.hpp"
 #include "ray.hpp"
 #include "sphere.hpp"
 
 int main(int argc, char* argv[]) {
-  constexpr auto aspect_ratio {16.0 / 9.0};
-  // camera
-  constexpr auto viewport_height {2.0};
-  constexpr auto viewport_width {aspect_ratio * viewport_height};
-  constexpr auto focal_length {1.0};
-  constexpr auto origin {ktp::Point{0.0, 0.0, 0.0}};
-  constexpr auto horizontal {ktp::Vector{viewport_width, 0.0, 0.0}};
-  constexpr auto vertical {ktp::Vector{0.0, viewport_height, 0.0}};
-  constexpr auto lower_left_corner {origin - horizontal / 2.0 - vertical / 2.0 - ktp::Vector(0.0, 0.0, focal_length)};
+  const ktp::Camera camera {};
   // image data
   ppm::PPMFileData data {};
   data.m_name = "render.ppm";
   data.m_width  = 2048;
-  data.m_height = static_cast<int>(data.m_width / aspect_ratio);
+  data.m_height = static_cast<int>(data.m_width / camera.aspectRatio());
   data.m_pixels.reserve(data.m_width * data.m_height);
   // world
   ktp::HittableList world {};
   world.add(std::make_shared<ktp::Sphere>(ktp::Point(0.0,    0.0, -1.0),   0.5));
   world.add(std::make_shared<ktp::Sphere>(ktp::Point(0.0, -100.5, -1.0), 100.0));
   // pixels
-  // ppm::generateTestImage(data);
   for (int j = data.m_height - 1; j >= 0; --j) {
     std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < data.m_width; ++i) {
       const auto u {static_cast<double>(i) / (data.m_width  - 1.0)};
       const auto v {static_cast<double>(j) / (data.m_height - 1.0)};
-      const ktp::Ray ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      const ktp::Ray ray {camera.getRay(u, v)};
       data.m_pixels.push_back(ktp::colorToPPM(ray.rayColor(world)));
     }
   }
